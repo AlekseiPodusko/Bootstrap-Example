76a0a3eced83cf17d56f76eb75a064798b693299
AlekseiPodusko <112948029+AlekseiPodusko@users.noreply.github.com>
2023-01-25 10:50:45 +0200

    Add files via upload

---
+++ Bootstrap_js/main.js
@@ -0,0 +1,21 @@
+(fubction() {
+    'use strict';
+    window.addEventListener('load',function(){
+        var forms=document.getElementsByClassName('myForm');
+            var validation=Array.prototype.filter.call(forms,function(form){
+                form.addEventListener ('submit', function(event){
+                    if (form.checkValidity()===false){
+                        event.preventDefalt();
+                        event.stopPropagation();
+                    }
+                    form.classList.add('was-validated');
+            }, false);
+        } );
+    } , false);
+})();
+$('.my-carousel').carousel({
+    interval: 4000
+});
+$(function(){
+    $('[data-toggle="tooltip"]').tooltip()
+})
